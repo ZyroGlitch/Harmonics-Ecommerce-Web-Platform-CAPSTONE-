e61a872a76a309854ebcd9dd040f21e10fbf7e27
ZyroGlitch <johnfordbuliag182@gmail.com>
2024-12-24 14:26:07 +0800

    Initial Project Implementation 12-24-2024

---
+++ resources/js/app.jsx
@@ -0,0 +1,19 @@
+import './bootstrap';
+
+import { createInertiaApp } from '@inertiajs/react'
+import { createRoot } from 'react-dom/client'
+
+// Import Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+// Import Bootstrap JS (optional for dynamic components like modals)
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+createInertiaApp({
+    resolve: name => {
+        const pages = import.meta.glob('./Pages/**/*.jsx', { eager: true })
+        return pages[`./Pages/${name}.jsx`]
+    },
+    setup({ el, App, props }) {
+        createRoot(el).render(<App {...props} />)
+    },
+})
